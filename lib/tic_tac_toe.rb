5149db322340a81696ad490a40925567cb6dab39
Josh Wroe <josh_wroe@hotmail.co.uk>
2018-10-17 10:09:55 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -18,58 +18,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(input)
- index = input.to_i - 1
- return index
- end
 
-
-def move(board, index, value)
-  puts value
-  puts "fff"
-  board[index] = value
-end
-
-def position_taken?(board, index)
-  if board[index] == 'X' || board[index] == "x" || board[index] ==  'O'|| board[index] == "o"
-    return true
-  else
-    return false
-  end
-end
-
-  
-
-
-def valid_move?(board, index)
-  if index.between?(0,8) && position_taken?(board, index) == false
-    return true
-    
-  else
-    
-    return false
-    
-  end
-  
-end
-
-
-def turn(board)
-  
-    puts("Please enter 1-9:")
-    input = gets.strip
-    index = input_to_index(input)
- 
-   if !valid_move?(board, index)
-     puts "please enter a valid input"
-     turn(board)
-     
-  else 
-     move(board, index)
-     display_board(board)
-     
-   end
- end
 
 
 
