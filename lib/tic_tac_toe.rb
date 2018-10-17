271a4356f60f9f91ae0b1857072abab4b3cea049
Josh Wroe <josh_wroe@hotmail.co.uk>
2018-10-17 09:47:39 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -43,10 +43,13 @@ end
 
 def valid_move?(board, index)
   if index.between?(0,8) && position_taken?(board, index) == false
+    puts "valid move is true"
     return true
     
   else
+    puts "valid move is false"
     return false
+    
   end
   
 end
