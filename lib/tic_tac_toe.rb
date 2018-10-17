896f73f93807b7fd83b4ae29c40bd74991707bd5
Josh Wroe <josh_wroe@hotmail.co.uk>
2018-10-17 09:36:08 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -37,7 +37,7 @@ def position_taken?(board, index)
   
 end
 
-def valid_move?(array, index)
+def valid_move?(board, index)
   if index.between?(0,8) && position_taken?(board, index) == false
     return true
     
