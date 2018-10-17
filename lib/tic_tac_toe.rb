f6dca611e329317009c69af7db4f4950453e1b47
Josh Wroe <josh_wroe@hotmail.co.uk>
2018-10-17 13:56:25 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -104,6 +104,8 @@ def won?(board)
     position_3 = board[win_index_3]
 
     if position_1 == "X" && position_2 == "X"  && position_3 == "X"
+      puts win_combinations
+      puts "this is the win combinations"
       return win_combinations
       
     elsif position_1 == "O" && position_2 == "O"  && position_3 == "O"
@@ -167,6 +169,16 @@ end
 def winner(board)
   
   if won?(board) != false
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
   win_index = won?(board)
   winning_letter = won?(board[0])
   return winning_letter
