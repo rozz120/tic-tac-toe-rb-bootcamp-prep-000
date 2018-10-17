540a16681e7e8828d093033e27defcb08787030c
Josh Wroe <josh_wroe@hotmail.co.uk>
2018-10-17 10:35:03 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -47,13 +47,7 @@ def move(board, index, value)
 end
 
 
-def turn(board)
-  
-    puts("Please enter 1-9:")
-    input = gets.strip
-    index = input_to_index(input)
-    board[0] == 'X'
-  end
+
 
 
 
